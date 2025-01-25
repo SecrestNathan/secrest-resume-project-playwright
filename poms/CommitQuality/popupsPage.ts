0ff967897f70bcfed9d1deea5433d44836904077
Nathan Secrest <secrestnathan@gmail.com>
2025-01-25 17:48:22 -0600

    Commit Quality Popups Page Implemented

--- poms/CommitQuality/popupsPage.ts
+++ poms/CommitQuality/popupsPage.ts
@@ -6,18 +6,28 @@ export class PopupPage {
     // URLs
     public readonly url = "https://commitquality.com/practice-random-popup"
 
-    // Initialize Locators
+    // Locators
+    public readonly randomPopup: Locator
 
     constructor(page: Page) {
         this.page = page
 
         // Construct Locators
+        this.randomPopup = this.page.locator('.overlay-content').filter({ hasText: 'Random Popup' }).getByRole('button')
     }
 
-    // Actions - Navigation
+    // Navigation
     public async goto() {
         await this.page.goto(this.url)
     }
 
-    // Actions - Exercise: Popups
+    // Actions
+    public async dismissPopupWhenItAppears() {
+        await this.page.addLocatorHandler(
+            this.page.getByText('Random Popup'),
+            async () => {
+                await this.randomPopup.click()
+            }
+        )
+    }
 }
--- tests/CommitQuality/popups.spec.ts
+++ tests/CommitQuality/popups.spec.ts
@@ -6,7 +6,15 @@ test.describe('Popups', () => {
     await popupPage.goto()
   })
 
-  test('', async ({ popupPage }) => {
-    
+  test('Popup Dismissal', async ({ popupPage }) => {
+    await popupPage.dismissPopupWhenItAppears()
+    // Pausing execution for 6 seconds. In a real scenario, you would have multiple tests happening, and
+    // as each happens the locator handler in dismissPopup() would check after every action if the popup
+    // has shown up, and then dismiss it. This manual wait is just to kill time and pretend some actions are
+    // happening. In this particular scenario, where the popup consitently shows up after some seconds, it'd
+    // actually be better practice to do something like waiting for the element to be visible and then dismissing
+    // it. That way there is no overhead from the locator handler checking after every action if the popup has appeared.
+    await new Promise((resolve) => setTimeout(resolve, 6000))
+    await expect(popupPage.randomPopup).not.toBeVisible()
   })
-})
+}) 
