8f0607dfbf75d733b08d534d6621277484e29b0e
Nathan Secrest <secrestnathan@gmail.com>
2025-02-01 20:41:22 -0600

    Commit Quality iFrames Implemented

--- poms/CommitQuality/iframesPage.ts
+++ poms/CommitQuality/iframesPage.ts
@@ -1,4 +1,4 @@
-import { Page, Locator } from "@playwright/test"
+import { Page, Locator, FrameLocator } from "@playwright/test"
 
 export class IframesPage {
     private readonly page: Page
@@ -7,17 +7,34 @@ export class IframesPage {
     public readonly url = "https://commitquality.com/practice-iframe"
 
     // Initialize Locators
+    private readonly iframeLocator: FrameLocator
+    private readonly filterInput: Locator
+    private readonly filterButton: Locator
+    public readonly productTable_NameColumn_Cells: Locator
 
     constructor(page: Page) {
         this.page = page
 
-        // Construct Locators
+        // Locators
+        this.iframeLocator = this.page.frameLocator('iframe')
+        this.filterInput = this.iframeLocator.getByRole('textbox')
+        this.filterButton = this.iframeLocator.locator('role=button', { hasText: 'Filter' })
+        this.productTable_NameColumn_Cells = this.iframeLocator.locator('table.product-list-table tbody tr td:nth-child(2)')
     }
 
-    // Actions - Navigation
+    // Navigation
     public async goto() {
         await this.page.goto(this.url)
     }
 
-    // Actions - Exercise: iFrames
+    // Actions
+    public async filterProducts(text: string) {
+        await this.filterInput.fill(text)
+        await this.filterButton.click()
+    }
+
+    // Getters
+    public async getResultsCount(rowColumnLocator: Locator) {
+        return await rowColumnLocator.count()
+    }
 }
--- tests/CommitQuality/iframes.spec.ts
+++ tests/CommitQuality/iframes.spec.ts
@@ -6,7 +6,11 @@ test.describe('iFrames', () => {
     await iframesPage.goto()
   })
 
-  test('', async ({ iframesPage }) => {
-    
+  test('Filter Table in iFrame and Assert Filter Worked', async ({ iframesPage }) => {
+    await iframesPage.filterProducts('Product 1')
+    const cellCount = await iframesPage.getResultsCount(iframesPage.productTable_NameColumn_Cells)
+    for (let i = 0; i < cellCount; i++) {
+      await expect(iframesPage.productTable_NameColumn_Cells.nth(i)).toHaveText('Product 1')
+    }
   })
 })
