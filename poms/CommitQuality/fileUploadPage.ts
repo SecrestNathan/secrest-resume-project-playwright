14ca29148c6e0419587b0dadc07ff7ffa9bf054f
Nathan Secrest <secrestnathan@gmail.com>
2025-02-03 20:40:31 -0600

    Commit Quality File Upload Implemented

--- poms/CommitQuality/fileUploadPage.ts
+++ poms/CommitQuality/fileUploadPage.ts
@@ -7,17 +7,27 @@ export class FileUploadPage {
     public readonly url = "https://commitquality.com/practice-file-upload"
 
     // Initialize Locators
+    public readonly fileUploadInput: Locator
+    public readonly submitButton: Locator
 
     constructor(page: Page) {
         this.page = page
 
         // Construct Locators
+        this.fileUploadInput = this.page.locator('.file-upload input[type="file"]')
+        this.submitButton = this.page.locator('role=button', { hasText: 'Submit' })
     }
 
-    // Actions - Navigation
+    // Navigation
     public async goto() {
         await this.page.goto(this.url)
     }
 
-    // Actions - Exercise: File Upload
+    // Actions
+    public async uploadFile(filePath: string) {
+        await this.fileUploadInput.setInputFiles(filePath)
+    }
+    public async clickSubmit() {
+        await this.submitButton.click()
+    }
 }
